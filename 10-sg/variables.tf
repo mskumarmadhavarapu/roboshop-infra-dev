b11064b5a1c667e206c44e4e71a59c3783f3969e
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-08 16:48:12 +0530

    infra-dev

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -5,3 +5,21 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "sg_names" {
+    type = list
+    default = [
+        # Databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # Backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # Backend ALB
+        "backend_alb",
+        # Frontend
+        "frontend",
+        # Frontend ALB
+        "frontend_alb",
+        # Bastion
+        "bastion"
+    ]
+}
