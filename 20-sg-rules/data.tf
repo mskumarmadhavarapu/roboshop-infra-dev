6e8b0e54470a74f8657e57ca468635f341b82e27
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-11 00:57:35 +0530

    roboshop-tf

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -21,3 +21,19 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 data "aws_ssm_parameter" "user_sg_id" {
   name  = "/${var.project}/${var.environment}/user_sg_id"
 }
+
+data "aws_ssm_parameter" "redis_sg_id" {
+  name  = "/${var.project}/${var.environment}/redis_sg_id"
+}
+
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name  = "/${var.project}/${var.environment}/rabbitmq_sg_id"
+}
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name  = "/${var.project}/${var.environment}/mysql_sg_id"
+}
+
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+  name  = "/${var.project}/${var.environment}/mbackend_alb_sg_id"
+}
