46d7e1ca323f56f23875b94662c3a10c055f4266
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-19 15:36:00 +0530

    roboshop-tf

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -35,7 +35,7 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
-  name    = "*.backend-alb.${var.environment}.${var.domain_name}"
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
   type    = "A"
 
  # load balance details
