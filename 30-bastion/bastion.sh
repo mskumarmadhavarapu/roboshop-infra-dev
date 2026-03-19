83b5077f0fbf6c0d4e7915d3c8fe5ff42cc37137
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-19 12:12:37 +0530

    roboshop-tf

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# We are creating 50GB root disk, but only 20GB is partitioned
+# Remaining 30GB we need to extend using below commands
+growpart /dev/nvme0n1 4
+lvextend -r -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
+
+
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
