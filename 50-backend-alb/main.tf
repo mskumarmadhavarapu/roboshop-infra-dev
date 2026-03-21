a683c1d249e5f5865676c92324858e83a7a9b2c8
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-21 19:50:45 +0530

    roboshop-tf

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -26,7 +26,7 @@ resource "aws_lb_listener" "http" {
     type = "fixed-response"
 
         fixed_response {
-        content_type = "text/plain"
+        content_type = "text/html"
         message_body = "<h1>Hi, I'm from HTTP Backend ALB</h1>"
         status_code  = "200"
         }
