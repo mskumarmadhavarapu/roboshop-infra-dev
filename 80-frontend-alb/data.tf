cc7e728021083180e8685ebb2bda9bee8f2fc32e
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-21 20:11:54 +0530

    roboshop-tf

--- 80-frontend-alb/data.tf
+++ 80-frontend-alb/data.tf
@@ -1,24 +1,3 @@
-data "aws_ami" "joindevops" {
-  most_recent      = true
-  owners           = ["973714476881"]
-
-  filter {
-    name   = "name"
-    values = ["Redhat-9-DevOps-Practice"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-
 data "aws_ssm_parameter" "frontend_alb_sg_id" {
   name  = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
