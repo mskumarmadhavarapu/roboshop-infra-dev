2229e92a958ffad0e0a07e5d3cf9cf6dfd907f15
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-26 21:33:05 +0530

    roboshop-tf

--- 80-frontend-alb/data.tf
+++ 80-frontend-alb/data.tf
@@ -2,8 +2,8 @@ data "aws_ssm_parameter" "frontend_alb_sg_id" {
   name  = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name  = "/${var.project}/${var.environment}/private_subnet_ids"
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
 }
 
 data "aws_ssm_parameter" "frontend_alb_certificate_arn" {
