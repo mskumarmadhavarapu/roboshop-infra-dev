f6bb6b3bb138f343b630564122c1ff9db67f5c7f
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-28 21:37:11 +0530

    roboshop-tf

--- 98-openvpn/main.tf
+++ 98-openvpn/main.tf
@@ -2,25 +2,14 @@ resource "aws_instance" "bastion" {
     ami                 = local.ami_id
     instance_type       = "t3.small"
     subnet_id           = local.public_subnet_id
-    vpc_security_group_ids = [local.bastion_sg_id]
-    user_data = file("bastion.sh")
-
-    root_block_device {
-      volume_size = 50
-      volume_type = "gp3"
-      # EBS volume tags
-        tags = merge(
-          {
-              Name = "${var.project}-${var.environment}-bastion"
-          },
-        local.common_tags
-        )
-    }
+    vpc_security_group_ids = [local.openvpn_sg_id]
+    user_data = file("vpn.sh")
     
     tags = merge(
         {
-            Name = "${var.project}-${var.environment}-bastion"
+            Name = "${var.project}-${var.environment}-openvpn"
         },
         local.common_tags,
     )
 }
+
